c9166e6e5e6b82cad1d8421685711eab922a637c
WanHuzairipipe <wan.huzairi@pipeline.com.my>
2021-07-21 12:33:28 +0800

    first

---
+++ database/factories/BelianFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\belian;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class BelianFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = belian::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
